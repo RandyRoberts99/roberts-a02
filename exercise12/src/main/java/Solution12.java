054e0eda96873999ea78bb52e7a362d50edfb649
RandyRo99 <83358766+RandyRo99@users.noreply.github.com>
2021-09-19 17:06:43 -0400

    Added pseudocode to exercise 12

--- exercise12/src/main/java/Solution12.java
+++ exercise12/src/main/java/Solution12.java
@@ -1,4 +1,11 @@
-import java.text.DecimalFormat;
+/*
+ *  UCF COP3330 Fall 2021 Assignment 2 Solution
+ *  Copyright 2021 Randall Roberts
+ */
+
+// Write a program that calculates compound interest given the principal, number of years,
+// and interest rate and print the result.
+
 import java.util.Scanner;
 
 public class Solution12
@@ -16,9 +23,12 @@ public class Solution12
         System.out.print("Enter the number of years: ");
         double numYears = reader.nextDouble();
 
+        printInterestTotal(principal, interestRate, numYears);
+    }
+    private static void printInterestTotal(double principal, double interestRate, double numYears)
+    {
         double compoundInterestTotal = principal * (1 + ((interestRate/100) * numYears));
 
         System.out.print("After " + (int)numYears + " years at " + interestRate + "%, the investment will be worth $" + (int)compoundInterestTotal + ".");
-        // After 4 years at 4.3%, the investment will be worth $1758.
     }
 }
