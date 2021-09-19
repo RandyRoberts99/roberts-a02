719d4dc35af216348617850fb349c914a9b7206a
RandyRo99 <83358766+RandyRo99@users.noreply.github.com>
2021-09-19 16:36:19 -0400

    Added pseudocode and organized exercise09

--- exercise09/src/main/java/Solution09.java
+++ exercise09/src/main/java/Solution09.java
@@ -1,3 +1,11 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 2 Solution
+ *  Copyright 2021 Randall Roberts
+ */
+
+// Write a program that takes in a length and width, calculates a ceiling area and
+// determines how many paint buckets will be needed for the ceiling, and prints the result.
+
 import java.util.Scanner;
 
 public class Solution09
@@ -9,9 +17,14 @@ public class Solution09
     {
         System.out.print("What is the length of your ceiling? ");
         int length = reader.nextInt();
+
         System.out.print("What is the width of your ceiling? ");
         int width = reader.nextInt();
 
+        printNumGallons(length, width);
+    }
+    private static void printNumGallons(int length, int width)
+    {
         int ceilArea = length * width;
         int numGallons = ceilArea / gallonAmount;
 
@@ -19,6 +32,6 @@ public class Solution09
         {
             numGallons++;
         }
-        System.out.print("You will need to purchase " + numGallons + " gallons of paint to cover "+ ceilArea + " square feet.");
+        System.out.print("You will need to purchase " + numGallons + " gallons of paint to cover " + ceilArea + " square feet.");
     }
 }
