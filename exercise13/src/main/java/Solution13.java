40797df5eb3d07f7fe5b5a5c227e2cf03ada71fc
RandyRo99 <83358766+RandyRo99@users.noreply.github.com>
2021-09-19 17:11:12 -0400

    Added pseudocode/edited exercise 13

--- exercise13/src/main/java/Solution13.java
+++ exercise13/src/main/java/Solution13.java
@@ -1,4 +1,10 @@
-import java.text.DecimalFormat;
+/*
+ *  UCF COP3330 Fall 2021 Assignment 2 Solution
+ *  Copyright 2021 Randall Roberts
+ */
+
+// Write a program that takes in four variables and calculates the compounded interest and prints the result.
+
 import java.util.Scanner;
 
 public class Solution13
@@ -19,6 +25,10 @@ public class Solution13
         System.out.print("What is the number of times the interest is compounded per year? ");
         double yearsCompounded = reader.nextDouble();
 
+        printCompoundedInterestTotal(principal, interestRate, numYears, yearsCompounded);
+    }
+    private static void printCompoundedInterestTotal(double principal, double interestRate, double numYears, double yearsCompounded)
+    {
         double compoundInterestTotal = principal * Math.pow(1 + (interestRate/100 / yearsCompounded), numYears * yearsCompounded);
 
         System.out.print("$" + (int)principal + " invested at " + String.format("%.1f", interestRate) + "% for " + (int)numYears + " years compounded " + (int)yearsCompounded + " times per year is $" + String.format("%.2f", compoundInterestTotal) + ".");
