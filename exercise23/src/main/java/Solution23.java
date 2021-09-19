964b4c593282bae4585c46283d1cf052f9976b1f
RandyRo99 <83358766+RandyRo99@users.noreply.github.com>
2021-09-19 16:01:43 -0400

    Edited exercise 22 and added exercise 23

---
+++ exercise23/src/main/java/Solution23.java
@@ -0,0 +1,77 @@
+import java.util.Scanner;
+
+public class Solution23
+{
+    private static final Scanner reader = new Scanner(System.in);
+
+    public static void main(String[] args)
+    {
+        String response1;
+        String response2;
+        String response3;
+        String response4;
+        String response5;
+
+        System.out.print("Is the car silent when you turn the key? ");
+        response1 = reader.next();
+
+        if (response1.equalsIgnoreCase("yes"))
+        {
+            System.out.print("Are the battery terminals corroded? ");
+            response2 = reader.next();
+
+            if (response2.equalsIgnoreCase("yes"))
+            {
+                System.out.print("Clean terminals and try starting again.");
+            }
+            else
+            {
+                System.out.print("Replace cables and try again.");
+            }
+        }
+        else
+        {
+            System.out.print("Does the car make a slicking noise? ");
+            response2 = reader.next();
+
+            if (response2.equalsIgnoreCase("yes"))
+            {
+                System.out.print("Replace the battery.");
+            }
+            else
+            {
+                System.out.print("Does the car crank up but fail to start? ");
+                response3 = reader.next();
+
+                if (response3.equalsIgnoreCase("yes"))
+                {
+                    System.out.print("Check spark plug connections.");
+                }
+                else
+                {
+                    System.out.print("Does the engine start and then die? ");
+                    response4 = reader.next();
+
+                    if (response4.equalsIgnoreCase("yes"))
+                    {
+                        System.out.print("Does you car have fuel injection? ");
+                        response5 = reader.next();
+
+                        if (response5.equalsIgnoreCase("yes"))
+                        {
+                            System.out.print("Get it in for service.");
+                        }
+                        else
+                        {
+                            System.out.print("Check to ensure the choke is opening and closing.");
+                        }
+                    }
+                    else
+                    {
+                        System.out.print("This should not be possible.");
+                    }
+                }
+            }
+        }
+    }
+}
