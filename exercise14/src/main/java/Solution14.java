31fc39a802ded7ae341c6445c0139a969172c1bb
RandyRo99 <83358766+RandyRo99@users.noreply.github.com>
2021-09-19 17:17:34 -0400

    Added pseudocode and edited exercise 14

--- exercise14/src/main/java/Solution14.java
+++ exercise14/src/main/java/Solution14.java
@@ -1,3 +1,11 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 2 Solution
+ *  Copyright 2021 Randall Roberts
+ */
+
+// Write a program that takes in an order amount, and state, and determines based on the state how much the total taxed
+// amount will be and prints the outcome.
+
 import java.util.Scanner;
 
 public class Solution14
@@ -13,6 +21,10 @@ public class Solution14
         System.out.print("What is the state? ");
         String state = reader.next();
 
+        printTaxedOrderAmount(orderAmount, state);
+    }
+    private static void printTaxedOrderAmount(double orderAmount, String state)
+    {
         double taxedOrderAmount = orderAmount;
 
         if (state.equals("WI"))
@@ -21,7 +33,6 @@ public class Solution14
             System.out.print("The subtotal is $" + String.format("%.2f", orderAmount) + "." + "\nThe tax is $" + String.format("%.2f", 10 * wisTaxRate) + "." + "\nThe total is $" + String.format("%.2f", taxedOrderAmount) + ".");
             System.exit(0);
         }
-
         System.out.print("The total is $" + String.format("%.2f", orderAmount) + ".");
     }
 }
