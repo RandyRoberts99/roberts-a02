e7019274744421193faddacb2d81b06abe7927d4
RandyRo99 <83358766+RandyRo99@users.noreply.github.com>
2021-09-19 16:28:49 -0400

    Added pseudocode and organized exercise08

--- exercise08/src/main/java/Solution08.java
+++ exercise08/src/main/java/Solution08.java
@@ -1,3 +1,11 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 2 Solution
+ *  Copyright 2021 Randall Roberts
+ */
+
+// Write a program that takes in 3 integers to represent people and pizza slices, and splits
+// each pizza slice evenly to give everyone an equal amount of pizza, and prints the results.
+
 import java.util.Scanner;
 
 public class Solution08
@@ -15,6 +23,10 @@ public class Solution08
         System.out.print("How many slices per pizza? ");
         int numPizzaSlices = reader.nextInt();
 
+        processOutput(numPeople, numPizzas, numPizzaSlices);
+    }
+    private static void processOutput(int numPeople, int numPizzas, int numPizzaSlices)
+    {
         System.out.println(numPeople + " people with " + numPizzas + " pizzas (" + (numPizzas * numPizzaSlices) + " slices)");
 
         int numPizzasLeftover = (numPizzas * numPizzaSlices) % numPeople;
