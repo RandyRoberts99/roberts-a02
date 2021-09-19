1c8887f1ce1b5a2eed37c115918f3d63d74c2c68
RandyRo99 <83358766+RandyRo99@users.noreply.github.com>
2021-09-19 16:16:29 -0400

    Added pseudocode and edited class exercise 06

--- exercise06/src/main/java/Solution06.java
+++ exercise06/src/main/java/Solution06.java
@@ -1,11 +1,19 @@
-import java.util.Calendar;
+/*
+ *  UCF COP3330 Fall 2021 Assignment 2 Solution
+ *  Copyright 2021 first_name last_name
+ */
+
+// Write a program that takes in an age and a retirement age, and calculates how long it will take to retire and
+// how much longer from the current year it will take to retire, and print the result.
+
 import java.util.Calendar;
 import java.util.Scanner;
 
 public class Solution06
 {
-    public static final Scanner reader = new Scanner(System.in);
-    public static final int currentYear = Calendar.getInstance().get(Calendar.YEAR);
+    private static final Scanner reader = new Scanner(System.in);
+    private static final int currentYear = Calendar.getInstance().get(Calendar.YEAR);
+
     public static void main(String[] args)
     {
         System.out.print("What is your age? ");
@@ -14,6 +22,10 @@ public class Solution06
         System.out.print("When would you like to retire? ");
         String retirementAge = reader.next();
 
+        calculateAndPrintOutput(age, retirementAge);
+    }
+    private static void calculateAndPrintOutput(String age, String retirementAge)
+    {
         int numAge = Integer.parseInt(age);
         int numRetirementAge = Integer.parseInt(retirementAge);
         int ageDiff = numRetirementAge - numAge;
